1f9ce8f6829a3f0eeec79f8cda39e76832a48d58
S-Delowar <sayed.buet97@gmail.com>
2020-03-04 07:04:58 +0600

    true-false null-undef double-triple done

---
+++ double-vs-triple-equals.js
@@ -0,0 +1,20 @@
+const first = 1;
+const second = true;
+
+//doubole equal check only equality of values
+if(first == second){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false")
+}
+
+//triple equal check equality of value and type
+if(first === second){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false")
+}
+
+//so for compare we will always use ===
---
+++ null-vs-undefined.js
@@ -0,0 +1,12 @@
+//undefined
+// let pakhi;
+// console.log(pakhi);
+
+function add(num1, num2){
+    return num1, num2;
+}
+const result = add(90);
+console.log(result);
+
+// const premik = {name:"smart-dude", phone: 47887};
+// console.log(premik.gf);
