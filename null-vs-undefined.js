0ef9d5d2a447f3c4311cdff36a678ace4af5ae03
S-Delowar <sayed.buet97@gmail.com>
2020-03-04 07:05:33 +0600

    true-false null-undef double-triple done

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -2,11 +2,25 @@
 // let pakhi;
 // console.log(pakhi);
 
-function add(num1, num2){
-    return num1, num2;
-}
-const result = add(90);
-console.log(result);
+// function add(num1, num2){
+//     return num1, num2;
+// }
+// const result = add(90);
+// console.log(result);
 
 // const premik = {name:"smart-dude", phone: 47887};
 // console.log(premik.gf);
+
+
+null
+//set kore newa hoi check korar jonno
+
+
+
+
+
+
+
+
+
+
