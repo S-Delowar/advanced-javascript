1e0ea053fabf131edbb6c4a9d63137912274645a
S-Delowar <sayed.buet97@gmail.com>
2020-03-04 06:33:35 +0600

    add

---
+++ true-false.js
@@ -0,0 +1,24 @@
+// 0 chara all numbers truthy. empty string falsey. sting er vitore kisu thakle truthy. undefined hole false. 
+
+//falsy
+//0
+//""
+//undefined
+//null
+//NaN
+//false
+
+//Truthy
+//'0'
+//' '
+//[]
+//{}
+//"false"
+
+let name = ""
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
